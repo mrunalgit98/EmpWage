d08b30ecc1038f02c7bdff7ee4612f27d69288c9
mrunal <mrunalbhoir100@gmail.com>
2022-07-12 12:15:27 +0530

    UC3 Part time

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,19 +1,20 @@
-//Calculate Daily Employee Wage
+//Add Part time Employee & Wage
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		//constant
 		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 		int EMP_RATE_PER_HOUR=20;
-		//variable
 		int empHrs=0;
 		int empWage=0;
 		double empCheck=Math.floor(Math.random()*10)%3;
-		if (empCheck==IS_FULL_TIME)
+		if(empCheck==IS_PART_TIME)
+			empHrs=4;
+		else if(empCheck==IS_FULL_TIME)
 			empHrs=8;
 		else
 			empHrs=0;
 		empWage=empHrs*EMP_RATE_PER_HOUR;
-			System.out.println("emp Wage -- " +empWage);
+			System.out.println("emp Wage--" +empWage);
 	}
 }

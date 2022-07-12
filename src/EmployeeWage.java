90b797d91dadb0c025356b5f453668ad23fbc75d
mrunal <mrunalbhoir100@gmail.com>
2022-07-12 12:13:16 +0530

    UC2 EmployeeWage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,4 +1,19 @@
+//Calculate Daily Employee Wage
 
 public class EmployeeWage {
-
+	public static void main(String[] args) {
+		//constant
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		//variable
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if (empCheck==IS_FULL_TIME)
+			empHrs=8;
+		else
+			empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+			System.out.println("emp Wage -- " +empWage);
+	}
 }

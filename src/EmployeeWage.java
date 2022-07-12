f6ae4add8dff5c4902bffab422478e081f1f5df4
mrunalgit98 <88592639+mrunalgit98@users.noreply.github.com>
2022-07-12 12:09:09 +0530

    Update EmployeeWage.java

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,4 +1,14 @@
+//Check Employee is Present or Absent
 
 public class EmployeeWage {
-
+	
+	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME)
+		System.out.println("employee is present");
+		else
+			System.out.println("employee is absent");
+	}
+	
 }
